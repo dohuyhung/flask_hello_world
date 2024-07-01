a4323bb9bf4c6a2452ea1aa04f8c53e869a24d62
Do Huy Hung <hungdh@doxanh.dev>
2024-07-01 14:58:38 +0700

    debug 4

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 ls
+# install python 3.11 on EC2. You should install it in separate steps. Do not include it in CodePipeline. Also, please set up a virtual environment.
+# dnf install python3.11 -y
+# follow this tutorial : https://plainenglish.io/community/how-to-install-python-3-11-with-pip-on-amazon-linux-2023-9ab2ed
+
 cd /opt/app/flask
-pip install -r requirements.txt
+python3.11 -m pip install -r requirements.txt
