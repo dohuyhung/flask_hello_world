71b65dd8073b8affd4990129348a2d15a03d5099
Do Huy Hung <hungdh@doxanh.dev>
2024-07-01 16:30:35 +0700

    debug 15

--- scripts/setup_nginx.sh
+++ scripts/setup_nginx.sh
@@ -1,1 +1,6 @@
-# copy your
+# Copy your Nginx configuration if you need to do so via a Bash script.
+# cd /tmp
+# cp -f /tmp/hello.conf /etc/nginx/conf.d
+
+# restart nginx service
+systemctl restart nginx
