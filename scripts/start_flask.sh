45f10daa9ab0efc10d2e2270dbc075d3f0a35cab
Do Huy Hung <hungdh@doxanh.dev>
2024-07-04 16:03:13 +0700

    debug env variable

--- scripts/start_flask.sh
+++ scripts/start_flask.sh
@@ -4,4 +4,6 @@ cd /opt/app
 
 # It depends on how you start or restart the Python app. Do it your way.
 # pkill -P1 gunicorn
+echo $MYNAME
+echo $MYPROFILESECRETKEY
 gunicorn -b 0.0.0.0 'hello:app' --daemon
